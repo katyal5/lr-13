8707ac9cabfa0a10f9368efc51a89f18447e25f5
katyal5 <118711183+katyal5@users.noreply.github.com>
2022-12-15 21:42:47 +0300

    Add files via upload

---
+++ 2.pas
@@ -0,0 +1,27 @@
+﻿var
+  myfile: text;
+  a: string;
+  i,ii, n, k: integer;
+begin
+ print ('Введите N и K');
+  read (n, k);
+  assign(myfile,'C:\text2.txt');
+  rewrite(myfile);
+  for i:= 1 to n do
+  begin
+    for ii:= 1 to k do begin
+      if ii< k then
+        write(myfile,'*')
+      else
+        writeln(myfile,'*')
+    end;
+    end;
+close(myfile);
+reset(myfile);
+  for i:= 1 to n do
+    begin
+      readln(myfile,a);
+      writeln(a);
+    end;
+  close(myfile);
+end.
